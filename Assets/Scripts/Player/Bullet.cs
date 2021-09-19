3c56201b3ea9ab8741afca48e4c03e8fe039900d
trong7a2002 <tienem123.com@gmail.com>
2021-09-19 13:57:29 +0700

    -chest fix
    -add chest mechanic

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -11,6 +11,7 @@ public class Bullet : MonoBehaviour
     public Rigidbody2D rigidbody2D;
 
     // Start is called before the first frame update
+
     void Start()
     {
         rigidbody2D.velocity = transform.right * speedBullet;
