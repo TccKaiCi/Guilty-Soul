9a7b005bb4f87831291f64e24db785eb1231479d
Chung <52872325+HiamKaito@users.noreply.github.com>
2021-09-19 15:43:17 +0700

    Revert "dowload assset free"

---
+++ Assets/Scripts/ChestController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestController : MonoBehaviour
+{
+    public bool isOpen = false;
+
+    public void OpenChest()
+    {
+        if( !isOpen )
+        {
+           
+            Debug.Log("Chest is open now...");
+
+        }
+
+    }
+}
