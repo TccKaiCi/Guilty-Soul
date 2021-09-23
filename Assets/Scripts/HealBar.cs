f4bd1538a28fb1101db5a681340f03bf6109ee1f
trong7a2002 <tienem123.com@gmail.com>
2021-09-23 14:15:43 +0700

    -add healbar interact
    add interaction of health bar from player  current stat and save file loader

--- Assets/Scripts/HealBar.cs
+++ Assets/Scripts/HealBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,25 +9,51 @@ public class HealBar : MonoBehaviour
     public GameObject ant;
     public Transform slotPanel;
     public Player player;
-    public int c_hp;
+    private int p_hp;
     public int c_mana;
+    GameObject instance;
     public void Start()
     {
-
+        //tham chieu player stat
         player = ant.GetComponent<Player>();
-        for (int i = 0; i < player.health; i++)
-        {
-            GameObject instance = Instantiate(slotPrefab);
-            instance.transform.SetParent(slotPanel);
-        }
-
+        //luu lai mau khoi dau
+        p_hp = player.health;
+        //tao mau
+        drawHpbar();
     }
     private void Update()
     {
-        Hpchange();
+        HpchangeChecker();
     }
-    public void Hpchange()
+
+    public void HpchangeChecker()
     {
        
+        if (p_hp != player.health)
+        {
+            reDraw();
+            Debug.Log("change hp");
+            drawHpbar();
+            p_hp = player.health;
+            
+        }
     }
+    public void drawHpbar()
+    {
+        
+        for (int i = 0; i < player.health; i++)
+        {
+           var instance = Instantiate(slotPrefab);
+            instance.transform.SetParent(slotPanel);
+          
+        }
+    }
+   public void reDraw()
+    {
+        foreach (Transform child in slotPanel)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+
 }
